68779136fed4190d7fac1c3d6dd8217521b0f77a
Eve0712 <junwoo.kim0712@gmail.com>
2022-08-29 21:20:25 -0700

    now able to record command and removed on release of lack of use

--- ktomcari.py
+++ ktomcari.py
@@ -19,52 +19,54 @@ def on_scroll(x, y, dx, dy):
         x, y, dx, dy))
 
 def on_press(key):
-    try:
-        print(key)
-        if str(key) == '':
-            logging.info('k\x03')
-        elif str(key) == '':
-            logging.info('k\x1a')
-        elif str(key) == '':
-            logging.info('k\x16')
-        else:
+    global globalDevice, keyboard_listener, mouse_listener
+    if str(key) == 'Key.esc':
+        if globalDevice == ('keyboard' or 'all'):
+            keyboard_listener.stop()
+        if globalDevice == ('mouse' or 'all'):
+            mouse_listener.stop()
+        return
+    if globalDevice == 'keyboard' or 'all':
+        try: 
+            if str(key.char) == '':
+                logging.info('^c')
+                return
+            elif str(key.char) == '':
+                logging.info('^z')
+                return
+            elif str(key.char) == '':
+                logging.info('^v')
+                return
             logging.info('k{0}'.format(
-                key.char))
-    except AttributeError:
-        logging.info('k{0}'.format(
-            key))
+                            key.char))
+        except AttributeError:
+            logging.info('k{0}'.format(
+                        key))
+    return
         
 def on_release(key):
-    if str(key) == '':
-            logging.info('k\x03')
-    elif str(key) == '':
-        logging.info('k\x1a')
-    elif str(key) == '':
-        logging.info('k\x16')
-    else: logging.info('k{0}'.format(
-        key))
-        
-    logging.info('r{0}'.format(
-        key))
-    if key == pynput.keyboard.Key.esc:
-        # Stop listener
-        return False
+    return
     
 def Recording(device='all'):
-    logging.basicConfig(filename='mouse_log.txt', 
+    global globalDevice, keyboard_listener, mouse_listener
+    
+    globalDevice = device
+    
+    logging.basicConfig(filename='log.txt', 
                         level=logging.DEBUG, 
                         format='%(message)s')
-    if device == 'all' or 'keyboard':
-        keyboard_listener = KeyboardListener(on_press=on_press,
-                                             on_release=on_release)
-    if device == 'all' or 'mouse':
+    keyboard_listener = KeyboardListener(on_press=on_press,
+                                            on_release=on_release)
+    if device == ('all' or 'mouse'):
         mouse_listener = MouseListener(on_move=on_move, 
                                     on_click=on_click, 
                                     on_scroll=on_scroll)
-    if device == 'all' or 'keyboard':keyboard_listener.start()
-    if device == 'all' or 'mouse':mouse_listener.start()
-    if device == 'all' or 'keyboard':keyboard_listener.join()
-    if device == 'all' or 'mouse':mouse_listener.join()
-
+    keyboard_listener.start()
+    if device == ('all' or 'mouse'):mouse_listener.start()
+    keyboard_listener.join()
+    if device == ('all' or 'mouse'):
+        mouse_listener.join()
+    print('recorded successfully')
+    return
 
 Recording('keyboard')
